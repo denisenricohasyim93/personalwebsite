c5acf6c0f3d3adbf79811d50743d2dbd1bca48ce
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-10-27 17:20:57 +0700

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
-import React, { Component } from 'react';
-import { Window, TitleBar, Text } from 'react-desktop/windows';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable flowtype/require-valid-file-annotation */
 
-class App extends Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Chip from 'material-ui/Chip';
+
+const styles = theme => ({
+  chip: {
+    margin: theme.spacing.unit / 2,
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+});
+
+class ChipsArray extends React.Component {
+  state = {
+    chipData: [
+      { key: 0, label: 'Angular' },
+      { key: 1, label: 'JQuery' },
+      { key: 2, label: 'Polymer' },
+      { key: 3, label: 'ReactJS' },
+      { key: 4, label: 'Vue.js' },
+    ],
+  };
+
+  styles = {
+    chip: {
+      margin: 4,
+    },
+    wrapper: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+  };
+
+  handleRequestDelete = data => () => {
+    if (data.label === 'ReactJS') {
+      alert('Why would you want to delete React?! :)'); // eslint-disable-line no-alert
+      return;
+    }
+
+    const chipData = [...this.state.chipData];
+    const chipToDelete = chipData.indexOf(data);
+    chipData.splice(chipToDelete, 1);
+    this.setState({ chipData });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Window
-          color={this.props.color}
-          theme={this.props.theme}
-          chrome
-          height="300px"
-          padding="12px"
-        >
-          <TitleBar title="My Windows Application" controls/>
-          <Text color={this.props.theme === 'dark' ? 'white' : '#333'}>Hello World</Text>
-        </Window>
+      <div className={classes.row}>
+        {this.state.chipData.map(data => {
+          return (
+            <Chip
+              label={data.label}
+              key={data.key}
+              onRequestDelete={this.handleRequestDelete(data)}
+              className={classes.chip}
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
-export default App;
+ChipsArray.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ChipsArray);
